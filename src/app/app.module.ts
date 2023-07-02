350104d4e774640b4e2c9df6ac6cd5b995e8d20f
Moh4medNajjar <najjarmohamed443@gmail.com>
2023-07-02 18:59:34 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,54 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ChartModule } from 'angular-highcharts';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { TableComponent } from './table/table.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    TableComponent,
+
+
   ],
   imports: [
+    ChartModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    FontAwesomeModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/side-nav/side-nav.component.ts
@@ -0,0 +1,171 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { faUsers,faPencil, faMessage,faSearch, faHome, faWrench, faHandPaper, faDiagramNext, faSignOut, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Chart } from 'angular-highcharts';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+import {MatNativeDateModule} from '@angular/material/core';
+import { DatePipe } from '@angular/common';
+import { oneLineBar } from './oneBarChart';
+
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.css'],
+
+
+
+})
+export class SideNavComponent {
+  faUsers = faUsers;
+  faUser = faUser;
+  faMessage = faMessage;
+  faHome = faHome;
+  faWrench = faWrench;
+  faHandPaper = faHandPaper;
+  faDiagramNext = faDiagramNext;
+  faSignOut = faSignOut;
+  faSearch = faSearch;
+  faPencil = faPencil
+
+
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+    chart = new Chart({
+      chart: {
+        styledMode: false,
+        backgroundColor:'transparent',
+        type: 'spline',
+        height: 325,
+        plotShadow:false,
+      },
+      title: {
+        text: 'Performance',
+        style: {
+        color: '#67748E',
+        fontWeight:'500'
+        }
+      },
+      xAxis: {
+        categories: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec'
+        ]
+      },
+      yAxis: {
+        title: {
+          text: 'Performance'
+        }
+      },
+      defs:{
+        gradient0: {
+          tagName: 'linearGradient',
+          id: 'gradient-0',
+          x1: 0,
+          y1: 0,
+          x2: 0,
+          y2: 1,
+          children: [
+            {
+              tagName: 'stop',
+              offset: 0
+            },
+            {
+              tagName: 'stop',
+              offset:0
+            }
+          ]
+
+        }
+      } as any,
+      series: [
+        { name: '',
+          marker: {
+          enabled: false
+        },
+          type: "areaspline",
+          color: '#603BAF',
+          opacity: 0.5,
+          data: [2, 60, 55, 70, 20, 40, 35, 60, 100, 70, 100, 256],
+          showInLegend: false,
+        },
+      ],
+      credits: {
+        enabled: false
+      }
+    });
+
+    donchart = new Chart({
+      chart: {
+        type: 'pie',
+        height: 300,
+        width: 300,
+      },
+      colors: [ '#BBACEF','#E2DEF7','#715DC6'],
+      accessibility: {
+        point: {
+          valueSuffix: '%'
+        }
+      },
+      title: {
+        text: 'Pourcentage'
+      },
+      subtitle: {
+
+        verticalAlign: 'middle',
+      floating: true,
+      text: '5,824,213',
+      style: {
+        fontWeight: '600',
+        fontSize: 15,
+        color:'#1E2838'
+      }
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.0f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          innerSize: '75%',
+          dataLabels: {
+            enabled:false},
+          showInLegend: false
+        }
+      },
+      series: [
+        {
+          type: "pie",
+          color: '#393479',
+          opacity: 1,
+          data: [30, 10,60],
+        },
+      ],
+      credits: {
+        enabled: false
+      }
+    });
+
+    bar = new Chart(oneLineBar)
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+  selected!: Date | null;
+}
